e09169710768d7bb09caae9dae9eaa6acf17250e
okafke <65917827+okafke@users.noreply.github.com>
2025-11-16 16:05:28 +0100

    feat(web): Regex pattern for debugging method calls

--- headlessmc-launcher/src/main/java/io/github/headlesshq/headlessmc/launcher/instrumentation/debug/DebugTransformer.java
+++ headlessmc-launcher/src/main/java/io/github/headlesshq/headlessmc/launcher/instrumentation/debug/DebugTransformer.java
@@ -2,11 +2,13 @@ package io.github.headlesshq.headlessmc.launcher.instrumentation.debug;
 
 import io.github.headlesshq.headlessmc.launcher.instrumentation.AbstractClassTransformer;
 import io.github.headlesshq.headlessmc.launcher.instrumentation.EntryStream;
+import org.jspecify.annotations.Nullable;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.*;
 
 import java.io.PrintStream;
 import java.lang.reflect.Modifier;
+import java.util.regex.Pattern;
 
 import static org.objectweb.asm.Opcodes.*;
 
@@ -14,6 +16,7 @@ import static org.objectweb.asm.Opcodes.*;
  * A transformer that prints the name of every method that is called.
  */
 public class DebugTransformer extends AbstractClassTransformer {
+    private final @Nullable Pattern debugPattern;
     private final long debugTime;
     private final boolean enabled;
     private final boolean onlyCurrentCall;
@@ -23,6 +26,8 @@ public class DebugTransformer extends AbstractClassTransformer {
         this.debugTime = Long.parseLong(System.getProperty("hmc.debug.transformer.wait.time", "0"));
         this.enabled = Boolean.parseBoolean(System.getProperty("hmc.debug.transformer.enabled", "true"));
         this.onlyCurrentCall = Boolean.parseBoolean(System.getProperty("hmc.debug.transformer.only.current.call", "false"));
+        String debugPattern = System.getProperty("hmc.debug.transformer.debug.pattern");
+        this.debugPattern = debugPattern == null ? null : Pattern.compile(debugPattern);
     }
 
     @Override
@@ -41,10 +46,14 @@ public class DebugTransformer extends AbstractClassTransformer {
             if (!onlyCurrentCall) {
                 while (node != null) {
                     if (node instanceof MethodInsnNode) {
+                        String debugInfo = ((MethodInsnNode) node).owner + "." + ((MethodInsnNode) node).name + ((MethodInsnNode) node).desc;
+                        if (debugPattern != null && !debugPattern.matcher(debugInfo).matches()) {
+                            continue;
+                        }
+
                         InsnList debug = new InsnList();
                         debug.add(new FieldInsnNode(GETSTATIC, Type.getInternalName(System.class), "out", Type.getDescriptor(PrintStream.class)));
-                        debug.add(new LdcInsnNode("Calling: " + ((MethodInsnNode) node).owner + "." + ((MethodInsnNode) node).name + ((MethodInsnNode) node).desc
-                                + " from " + cn.name + "." + mn.name + mn.desc));
+                        debug.add(new LdcInsnNode("Calling: " + debugInfo + " from " + cn.name + "." + mn.name + mn.desc));
                         debug.add(new MethodInsnNode(INVOKEVIRTUAL, Type.getInternalName(PrintStream.class), "println", "(Ljava/lang/String;)V", false));
                         if (debugTime != 0L) {
                             debug.add(new LdcInsnNode(debugTime));
@@ -63,16 +72,19 @@ public class DebugTransformer extends AbstractClassTransformer {
                 }
             }
 
-            InsnList debug = new InsnList();
-            debug.add(new FieldInsnNode(GETSTATIC, Type.getInternalName(System.class), "out", Type.getDescriptor(PrintStream.class)));
-            debug.add(new LdcInsnNode(cn.name + "." + mn.name + mn.desc));
-            debug.add(new MethodInsnNode(INVOKEVIRTUAL, Type.getInternalName(PrintStream.class), "println", "(Ljava/lang/String;)V", false));
-            if (debugTime != 0L) {
-                debug.add(new LdcInsnNode(debugTime));
-                debug.add(new MethodInsnNode(INVOKESTATIC, Type.getInternalName(Thread.class), "sleep", "(J)V", false));
-            }
+            String debugInfo = cn.name + "." + mn.name + mn.desc;
+            if (debugPattern == null || debugPattern.matcher(debugInfo).matches()) {
+                InsnList debug = new InsnList();
+                debug.add(new FieldInsnNode(GETSTATIC, Type.getInternalName(System.class), "out", Type.getDescriptor(PrintStream.class)));
+                debug.add(new LdcInsnNode(debugInfo));
+                debug.add(new MethodInsnNode(INVOKEVIRTUAL, Type.getInternalName(PrintStream.class), "println", "(Ljava/lang/String;)V", false));
+                if (debugTime != 0L) {
+                    debug.add(new LdcInsnNode(debugTime));
+                    debug.add(new MethodInsnNode(INVOKESTATIC, Type.getInternalName(Thread.class), "sleep", "(J)V", false));
+                }
 
-            mn.instructions.insert(debug);
+                mn.instructions.insert(debug);
+            }
             //mn.visitMaxs(0, 0);
         }
     }
